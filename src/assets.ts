6505184b6e86c9c43613c29fb94279099e62d4e8
Francois roland <f.roland@applicaster.com>
2021-12-13 22:57:40 +0100

    fix: image path

--- src/assets.ts
+++ src/assets.ts
@@ -1,19 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-export const logo = require("../assets/pedal-logo.png");
+export const logo = require(__dirname + "/../assets/pedal-logo.png");
 
-export const ts808Image = require("../assets/pedals/ts808.webp");
+export const ts808Image = require(__dirname + "/../assets/pedals/ts808.webp");
 
-export const bluesDriverImage = require("../assets/pedals/boss_blues_driver_bd-2w.webp");
+export const bluesDriverImage = require(__dirname + "/../assets/pedals/boss_blues_driver_bd-2w.webp");
 
-export const greenBigMuffImage = require("../assets/pedals/electro_harmonix_green_big_muff.webp");
+export const greenBigMuffImage = require(__dirname + "/../assets/pedals/electro_harmonix_green_big_muff.webp");
 
-export const morningGloryImage = require("../assets/pedals/jhs_morning_glory_v4.webp");
+export const morningGloryImage = require(__dirname + "/../assets/pedals/jhs_morning_glory_v4.webp");
 
-export const dmDriveImage = require("../assets/pedals/keeley_electronics_d&m_drive.webp");
+export const dmDriveImage = require(__dirname + "/../assets/pedals/keeley_electronics_d&m_drive.webp");
 
-export const fuzzBenderImage = require("../assets/pedals/keeley_electronics_fuzz_bender.webp");
+export const fuzzBenderImage = require(__dirname + "/../assets/pedals/keeley_electronics_fuzz_bender.webp");
 
-export const distPlusImage = require("../assets/pedals/mxr_distortion+.webp");
+export const distPlusImage = require(__dirname + "/../assets/pedals/mxr_distortion+.webp");
 
-export const ord1Image = require("../assets/pedals/nobels_odr-1.webp");
+export const ord1Image = require(__dirname + "/../assets/pedals/nobels_odr-1.webp");
