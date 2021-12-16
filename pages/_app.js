e359eeb93dac632964cbe267f444cc524d539441
Francois roland <f.roland@applicaster.com>
2021-12-16 19:00:41 +0100

    feat: opengraph headers

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,10 @@ export default function MyApp({ Component, pageProps }) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        <meta property="og:title" content="Tone Graphs" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.tonegraphs.com/" />
+        <meta property="og:image" content="/tone-graph-logo.png" />
       </Head>
       <Component {...pageProps} />
     </>
