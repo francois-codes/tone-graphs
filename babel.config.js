95eadf6482336d7ddcddae2cec8b6ae2dc421514
Francois roland <f.roland@applicaster.com>
2021-12-17 07:49:07 +0100

    chore: add class properties babel transform

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 // @generated: @expo/next-adapter@2.1.52
 // Learn more: https://docs.expo.dev/guides/using-nextjs/
 
-module.exports = { presets: ["@expo/next-adapter/babel", "@babel/preset-typescript"] };
+module.exports = {
+  plugins: [["@babel/plugin-proposal-class-properties", { loose: true }]],
+  presets: ["@expo/next-adapter/babel", "@babel/preset-typescript"],
+};
