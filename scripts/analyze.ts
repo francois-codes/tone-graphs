24c7fb9480f84c183b6820a49f6ab9087c811398
Francois roland <f.roland@applicaster.com>
2021-12-23 09:15:34 +0100

    feat: add analyze csv script

--- scripts/analyze.ts
+++ scripts/analyze.ts
@@ -7,44 +7,20 @@ import { writeFile } from "./utils";
 const path = require("path");
 const fs = require("fs");
 const R = require("ramda");
-import loess from "./interpolators/science";
-import akima from "./interpolators/akima";
-import cardinalSpline from "./interpolators/cardinalSpline";
 import averagePoint from "./interpolators/averagePoint";
 
-const inspect = (msg) => R.tap((x) => console.log(msg, x));
-
-import { formatDataPoints, getRelativeData, normalize, parseFile, reducePoints, toXYPairs } from "./utils";
+import {
+  inspect,
+  formatDataPoints,
+  getRelativeData,
+  normalize,
+  parseFile,
+  reducePoints,
+  toXYPairs,
+  countPoints,
+} from "./utils";
 
 const args = process.argv.slice(2);
-
-function getChunkSize(length) {
-  if (length <= 20) return length;
-  if (length <= 50) return Math.floor(length / 4);
-  if (length <= 100) return Math.floor(length / 16);
-  if (length <= 200) return Math.floor(length / 32);
-  return Math.floor(length / 64);
-}
-
-function countPoints(points: RawDataPoints) {
-  const steps = [0, 200, 800, 2000, 5000, 10000, 20000];
-
-  const pointsPerRange = R.groupBy(([x, y]) => {
-    const stepIndex = R.findIndex((step) => x <= step, steps);
-    return steps[stepIndex - 1];
-  }, points);
-
-  return R.compose(
-    R.reduce(R.concat, []),
-    R.map((range) => {
-      const rangeLength = range.length;
-      if (rangeLength <= 30) return range;
-      const chunkSize = getChunkSize(rangeLength);
-      return R.compose(R.map(R.last), R.splitEvery(chunkSize))(range);
-    }),
-    R.values,
-  )(pointsPerRange);
-}
 
 async function run() {
   const [dataFolder] = args;
@@ -66,13 +42,12 @@ async function run() {
         countPoints,
         normalize,
         getRelativeData(pinkNoiseRawData),
-        R.identity,
       )(rawDataPoints);
     }, plotFiles),
   );
 
   const jsonData = R.flatten(data);
-  await writeFile("data-akima.json", folderPath, jsonData);
+  await writeFile("data.json", folderPath, jsonData);
 }
 
 run();
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -7,10 +7,11 @@ const SLASH = "/";
 const CARRIAGE_RETURN = "\r";
 const TAB_BREAK = "\t";
 const COMA = ",";
+const PERCENT = "%";
 const EMPTY_STRING = "";
 const DOT = ".";
 const DASH = "-";
-const MAX_FREQUENCY = 20_000;
+const MAX_FREQUENCY = 20_500;
 const UTF_8 = "utf8";
 const PINK_NOISE_FILE_NAME = "pink noise";
 const DATA_ROW_KEYS = ["frequency", "db", "tone", "gain"];
@@ -60,7 +61,7 @@ export function writeFile(fileName: string, folderPath: string, data: string | u
   return fs.promises.writeFile(path.join(folderPath, fileName), stringifyIfNeeded(data));
 }
 
-const formatLabel = (prefix, value) => `${value.replace(prefix, EMPTY_STRING)}%`;
+const formatLabel = (prefix, value) => (value.includes("NA") ? "NA" : `${value.replace(prefix, EMPTY_STRING)}%`);
 
 export function getToneGainValue(fileName: string): { tone: string; gain: string } {
   const fileWithoutExtension = fileName.split(DOT)[0];
@@ -113,18 +114,55 @@ export async function extractDataFromFile(
   pinkNoiseData: RawDataPoints,
 ): Promise<ObjectData> {
   const rawDataPoints = await parseFile(fileName, folderPath);
-
   const relativeDataPoints = getRelativeData(rawDataPoints, pinkNoiseData);
-
   const normalizedData = normalize(relativeDataPoints);
-
   const reducedNormalizedData = reducePoints(normalizedData);
-
-  // const interpolatedPoints = interpolate(lines);
-
-  // console.log({ interpolatedPoints });
-
   const zippedData = toXYPairs(reducedNormalizedData);
 
   return formatDataPoints(zippedData, fileName);
+}
+
+function getChunkSize(length) {
+  if (length <= 20) return length;
+  if (length <= 50) return Math.floor(length / 4);
+  if (length <= 100) return Math.floor(length / 16);
+  if (length <= 200) return Math.floor(length / 32);
+  return Math.floor(length / 64);
+}
+
+export function countPoints(points: RawDataPoints) {
+  const steps = [0, 200, 800, 2000, 5000, 10000, 20000];
+
+  const pointsPerRange = R.groupBy(([x, y]) => {
+    const stepIndex = R.findIndex((step) => x <= step, steps);
+    return steps[stepIndex - 1];
+  }, points);
+
+  return R.compose(
+    R.reduce(R.concat, []),
+    R.map((range) => {
+      const rangeLength = range.length;
+      if (rangeLength <= 30) return range;
+      const chunkSize = getChunkSize(rangeLength);
+      return R.compose(R.map(R.last), R.splitEvery(chunkSize))(range);
+    }),
+    R.values,
+  )(pointsPerRange);
+}
+
+export const inspect = (msg) => R.tap((x) => console.log(msg, x));
+
+export function csvToGroupedDataPoints(csv: string): Record<string, RawDataPoints> {
+  return R.compose(
+    R.mapObjIndexed(R.map(({ frequency, db }) => [Number(frequency), Number(db)])),
+    R.groupBy(R.prop("settings")),
+    R.reject(({ frequency, db }) => frequency === 0 || db === 0),
+    R.map(([frequency, db, _, gain, tone]) => ({
+      frequency,
+      db,
+      settings: `t${tone?.replace(PERCENT, EMPTY_STRING) ?? "NA"}-g${gain?.replace(PERCENT, EMPTY_STRING) ?? "NA"}`,
+    })),
+    R.map(R.split(COMA)),
+    R.split(LINE_BREAK),
+  )(csv);
 }
